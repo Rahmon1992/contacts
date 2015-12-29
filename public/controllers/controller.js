7b4715e435eadd0bc276161064c19657333e770b
Abdurahmon Anvarov <rakhmon.anvarov@gmail.com>
2015-12-29 22:35:30 +0500

    First Commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,51 @@
+angular.module('listApp', [])
+.directive('tableData', [ '$http', function($http){
+	return {
+		restrict:'E',
+		replace:true,
+		templateUrl:'templates/table-data.html',
+		link:function($scope, element, attrs){
+
+			var refresh = function() {
+				$http.get('/contactlist')
+				.success(function(response) {
+					$scope.contactlist = response;
+					$scope.contact = "";
+				});
+			};
+
+			refresh();	
+
+			$scope.addContact = function() {
+				$http.post('/contactlist', $scope.contact).success(function(response) {
+					console.log(response);
+					refresh();
+				});
+			};
+
+			$scope.remove = function(id) {
+				console.log(id);
+				$http.delete('/contactlist/' + id).success(function(response) {
+					refresh();
+				});
+			};
+
+			$scope.edit = function(id) {
+				$http.get('/contactlist/' + id).success(function(response) {
+					$scope.contact = response;
+				});
+			};
+
+			$scope.update = function() {
+				$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+					refresh();
+				});
+			};
+
+			$scope.deselect = function() {
+				$scope.contact = "";
+			};
+			
+		}
+	}
+}]);
